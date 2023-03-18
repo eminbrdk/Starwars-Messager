62a8fc2d530095a58147722b5e7ba4f9ae42d357
emininmac <mamihaha4@gmail.com>
2023-03-18 12:21:51 +0300

    change selection in cell

--- Starwars-Messager/Costom Views/MessageCell.swift
+++ Starwars-Messager/Costom Views/MessageCell.swift
@@ -14,9 +14,14 @@ class MessageCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        selectionStyle = .none
     }
 }
